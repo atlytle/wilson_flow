97e0006f6532e19ca4bee171ebcabb1168f5b4a1
Andrew Lytle <atlytle@gmail.com>
2014-04-10 14:13:15 +0530

    Update step: bosonic force.

--- su3.py
+++ su3.py
@@ -7,6 +7,8 @@ nc = 3
 
 adj = lambda x: np.transpose(np.conj(x))  # Adjoint.
 
+
+# Could just store all indexing info and avoid function calls?
 def index(x,y,z,t,mu):
     "Linear index corresponding to link (x,y,z,t,mu)."
     x,y,z,t = x%nx, y%ny, z%nz, t%nt  # Periodic BCs.
@@ -22,6 +24,25 @@ def ix(xvec, mu):
     "Linear index corresponding to link (x,y,z,t,mu)."
     x,y,z,t = xvec[0]%nx, xvec[1]%ny, xvec[2]%nz, xvec[3]%nt  # Periodic BCs.
     return index(x,y,z,t,mu)
+    
+def fx(i):
+    "Link coordinates (x,y,z,t), mu corresponding to linear index i."
+    mu = i/Nsite
+    i = i-mu*Nsite
+    eo, i = i/(Nsite/2), i%(Nsite/2)  # Which checkerboard.
+    t,i = i/(nz*ny*nx/2), i%(nz*ny*nx/2)
+    z, i = i/(ny*nx/2), i%(ny*nx/2)
+    y, i = i/(nx/2), i%(nx/2)  # try divmod.
+    x = 2*i 
+    if not eo and (t+z+y) % 2 == 1: x +=1
+    if eo and (t+z+y) % 2 == 0: x +=1
+
+    return (x,y,z,t), mu
+    
+def test_index():
+    "Consistency of ix() and fx()."
+    for i in range(4*Nsite):
+        assert i == ix(*fx(i))
 
 def plaq(config):
     "Calculate plaquette values on a configuration."
@@ -146,6 +167,45 @@ def Fsq(config):
                 Fsq += np.dot(tmp, tmp)
                 
     return np.trace(Fsq)/Nsite/6
+    
+def Z(config):
+    '''Force matrix used to update configurations in Wilson Flow.
+    
+    Z(x, mu) = \partial_{x,mu} \sum_p plaq.'''
+    
+    # Initialize result.
+    result = np.zeros(config.shape, dtype=complex)
+    
+    for i in range(config.shape[0]):
+        m1 = config[i]  # U_mu(x).
+        xvec, mu = fx(i)
+        mh = muhat(mu)
+        d = [0,1,2,3]
+        d.remove(mu)  # nu != mu
+        Zxmu = np.zeros((3,3), dtype=complex)
+        for nu in d:
+            nh = muhat(nu)
+            # Staple up.
+            m2 = config[ix(xvec+mh, nu)]
+            m3 = adj(config[ix(xvec+nh, mu)])
+            m4 = adj(config[ix(xvec, nu)])
+            su = reduce(np.dot, [m1,m2,m3,m4])
+            # Staple down.
+            m5 = adj(config[ix(xvec-nh+mh, nu)])
+            m6 = adj(config[ix(xvec-nh, mu)])
+            m7 = config[ix(xvec-nh, nu)]
+            sd = reduce(np.dot, [m1,m5,m6,m7])
+            
+            Zxmu -= Hproject(su + sd)
+            
+        result[i] = Zxmu
+    return result
+    
+def Hproject(M):
+    "Convert M to a traceless anti-Hermitian matrix."
+    tmp = M-adj(M)
+    return tmp/2. - np.trace(tmp)/6.
+            
     
     
 def test_su3(config):
--- wflow.py
+++ wflow.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 from read_su3 import readfort, reconstruct
-from su3 import plaq, plaqt, plaqs, test_su3, F, Fsq
+from su3 import plaq, plaqt, plaqs, test_su3, F, Fsq, Z
 
 nx,ny,nz,nt = 16, 16, 16, 16
 Nsite = nx*ny*nz*nt
@@ -15,13 +15,14 @@ def main(files):
     dat = reconstruct(dat)
     print dat.shape
     #print test_su3(dat)
-    print 'plaq', plaq(dat)
-    print 'plaqt', plaqt(dat)
-    print 'plaqs', plaqs(dat)
+    #print 'plaq', plaq(dat)
+    #print 'plaqt', plaqt(dat)
+    #print 'plaqs', plaqs(dat)
     #tmp = F(dat, (0,0,0,0), 0, 1)
     #print tmp
     #print np.dot(tmp, tmp)
     #print Fsq(dat)
+    Z(dat)
     
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
