fa53b3792d0e9bdf14bdb3d467140e00bc58ef1e
Andrew <atlytle@gmail.com>
2014-08-01 16:37:12 +0530

    Further testing.

--- wflow.py
+++ wflow.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from read_su3 import readfort, reconstruct
 from su3 import plaq, plaqt, plaqs, test_su3, F, Fsq, Z
+from expsu3 import expsu3
 
 nx,ny,nz,nt = 16, 16, 16, 16
 Nsite = nx*ny*nz*nt
@@ -14,15 +15,20 @@ def main(files):
     dat = readfort(files[0])
     dat = reconstruct(dat)
     print dat.shape
-    #print test_su3(dat)
+    print test_su3(dat)
     #print 'plaq', plaq(dat)
-    #print 'plaqt', plaqt(dat)
-    #print 'plaqs', plaqs(dat)
-    #tmp = F(dat, (0,0,0,0), 0, 1)
+    
+    print 'plaqt', plaqt(dat)
+    print 'plaqs', plaqs(dat)
+    tmp = F(dat, (0,0,0,0), 0, 1)
     #print tmp
     #print np.dot(tmp, tmp)
-    #print Fsq(dat)
-    Z(dat)
+    print Fsq(dat)
+    
+    #dt = .01
+    #z = Z(dat)
+    #expZ = map(expsu3, -1j*dt*z)
+    #print test_su3(expZ)
     
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
