ec800f9cebc57823170d4085202de6cc51b0c4c1
Andrew <atlytle@gmail.com>
2014-08-06 18:06:18 +0530

    Find roots via bisection.

--- analyze_wflow.py
+++ analyze_wflow.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('../tifr')
 import numpy as np
 import pylab as p
+from scipy.interpolate import interp1d
 
 from parse_wflow import file2numpy
 from resample import JK_block, JKsigma
@@ -28,6 +29,26 @@ def dfb(ns):
     
     return result
 
+def broot(f, xi, xf, tol=0.0001):
+    '''Find a root of f(x) in [xi,xf] via bisection.
+    '''
+    s = np.sign(f(xi))
+    # Make sure f changes sign on the interval.
+    assert s != np.sign(f(xf))
+    # Check if the endpoints are roots.
+    if abs(f(xi)) < tol:
+        return xi
+    if abs(f(xf)) < tol:
+        return xf
+    while True:
+        mp = (xi+xf)/2
+        if abs(f(mp)) < tol:
+            return mp
+        if np.sign(f(mp)) == s:
+            xi = mp
+        else:
+            xf = mp
+
 def plot_items(items):
     """Generic plot routine.
     """
@@ -41,27 +62,27 @@ def main(argv):
     dat1 = file2numpy(inname(16,5.2875,0.025))
     dat2 = file2numpy(inname(16, 5.4, 0.025))
     nconf, nstep, nmeas = dat1.shape
-    #print dat2.shape
-    #print dat1[0,:,1]
-    #print dat1[0,:,2]
-    plot_items([dat1[x,:,2] for x in range(1,nconf)])#, dat1[0,:,2], dat2[0,:,1], dat2[0,:,2]])
+
+    #plot_items([dat1[x,:,2] for x in range(1,nconf)])#, dat1[0,:,2], dat2[0,:,1], dat2[0,:,2]])
 
     #JKblock
     t2E = JK_block(dat1[1:,:,2])
-    print t2E[0]
-    print JKsigma(t2E)
+    #print JKsigma(t2E)
     t = np.linspace(0,1.0,nstep)
-    plot_items([t2E[0], 100*t*dfb(t2E[0])])
+    #plot_items([t2E[0], 100*t*dfb(t2E[0])])
     
     nconf = dat2.shape[0]
-    plot_items([dat2[x,:,2] for x in range(1,nconf)])
+    #plot_items([dat2[x,:,2] for x in range(1,nconf)])
     t2E_2 = JK_block(dat2[:,:,2])
-    plot_items([t2E_2[0], 100*t*dfb(t2E_2[0])])
+    #plot_items([t2E_2[0], 100*t*dfb(t2E_2[0])])
 
     #plot
     #extract t0
+    f=interp1d(t,t2E[0]-0.3, kind='cubic')
+    g=interp1d(t, 100*t*dfb(t2E[0])-0.3, kind='cubic')
+    print broot(f,0.01,0.99)
+    print broot(g,0.01,0.99)
     #extract w0
-    #dfb(np.array([1,3,3,4,7,6]))
     return 0
     
 if __name__ == "__main__":
