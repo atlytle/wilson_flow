f0e293b95ffacd40645e3e29dde423a164ca00a8
Andrew <atlytle@gmail.com>
2014-08-19 13:59:12 +0530

    Beta=5.96 and 6.17 observables.

--- purgauge.py
+++ purgauge.py
@@ -3,9 +3,11 @@
 
 import sys
 import numpy as np
+from scipy.interpolate import interp1d
 
 from parse_wflow import convert_block as cb
 from tifr.resample import JK_block, JKsigma
+from analyze_wflow import plot_items, dfb2, broot
 
 def inname(B, conf):
     root = "/Users/atlytle/Documents/wflow_out"
@@ -26,8 +28,35 @@ def main(argv):
             blocks.append(f.readlines())
     d596 = np.array(map(cb, blocks))
 
-    print d596.shape
-    print d617.shape
+    dt = 0.01
+
+    # Analyze Beta = 5.96
+    nconf, nstep, nmeas = d596.shape
+    t = np.linspace(0,dt*nstep,nstep)
+    #plot_items([d596[x,:,4] for x in range(nconf)])
+    t2Ep = JK_block(d596[:,:,2])  # Ep = E from plaquette.
+    t2Ec = JK_block(d596[:,:,4])  # Ec = E from clover term.
+    dt2Ep = t*dfb2(t2Ep[0])/dt  # Logarithmic derivative.
+    dt2Ec = t*dfb2(t2Ec[0])/dt
+    plot_items([t2Ep[0], t2Ec[0], dt2Ep, dt2Ec])
+    for s in t2Ep[0], t2Ec[0], dt2Ep, dt2Ec:
+        f = interp1d(t, s-0.3, kind='cubic')
+        print np.sqrt(broot(f, 0.0, nstep*dt))
+
+    # Analyze Beta = 6.17
+    dt = 0.01
+    nconf, nstep, nmeas = d617.shape
+    t = np.linspace(0,dt*nstep,nstep)
+    #plot_items([d617[x,:,4] for x in range(nconf)])
+    t2Ep = JK_block(d617[:,:,2])  
+    t2Ec = JK_block(d617[:,:,4])  
+    dt2Ep = t*dfb2(t2Ep[0])/dt  
+    dt2Ec = t*dfb2(t2Ec[0])/dt
+    plot_items([t2Ep[0], t2Ec[0], dt2Ep, dt2Ec])
+    for s in t2Ep[0], t2Ec[0], dt2Ep, dt2Ec:
+        f = interp1d(t, s-0.3, kind='cubic')
+        print np.sqrt(broot(f, 0.0, nstep*dt))
+
     return 0
 
 if __name__ == "__main__":
