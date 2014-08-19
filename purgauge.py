f7e68b3a9b1bf1610f1fd680e7094a302961ba5d
Andrew <atlytle@gmail.com>
2014-08-19 12:04:27 +0530

    Correction.

--- purgauge.py
+++ purgauge.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 
 from parse_wflow import convert_block as cb
-from tifr.resample import JKsigma
+from tifr.resample import JK_block, JKsigma
 
 def inname(B, conf):
     root = "/Users/atlytle/Documents/wflow_out"
@@ -16,13 +16,18 @@ def main(argv):
     # Read in data.
     blocks = []
     for conf in 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500:
-        with open(inname(6.17, 5500), 'r') as f:
+        with open(inname(6.17, conf), 'r') as f:
             blocks.append(f.readlines())
-
-    blocks = np.array(map(cb, blocks))
+    d617 = np.array(map(cb, blocks))
     
-    print blocks.shape
+    blocks = []
+    for conf in 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000:
+        with open(inname(5.96, conf), 'r') as f:
+            blocks.append(f.readlines())
+    d596 = np.array(map(cb, blocks))
 
+    print d596.shape
+    print d617.shape
     return 0
 
 if __name__ == "__main__":
