4b3cb7001632ae036189328520aa6f6da8397ae6
Andrew <atlytle@gmail.com>
2014-08-19 11:59:36 +0530

    Read data.

--- purgauge.py
+++ purgauge.py
@@ -1,16 +1,27 @@
-"""
+"""Analysis of Saumen's pure gauge Wilson flow observables.
 """
 
 import sys
+import numpy as np
 
 from parse_wflow import convert_block as cb
+from tifr.resample import JKsigma
 
 def inname(B, conf):
     root = "/Users/atlytle/Documents/wflow_out"
-    return root + '/bt{0}/fort.{1}'.format(B, conf)
+    return root + '/bt{0}/flow.{1}'.format(B, conf)
 
 def main(argv):
-    print inname(6.17, 1000)
+
+    # Read in data.
+    blocks = []
+    for conf in 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500:
+        with open(inname(6.17, 5500), 'r') as f:
+            blocks.append(f.readlines())
+
+    blocks = np.array(map(cb, blocks))
+    
+    print blocks.shape
 
     return 0
 
